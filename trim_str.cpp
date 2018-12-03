1de08c492df2c7fafdb960f1faa1e36ee935f6ca
Eichhorn Moritz <moritz.eichhorn@outlook.com>
2018-12-03 13:30:40 +0100

    More changes

--- trim_str.cpp
+++ trim_str.cpp
@@ -5,13 +5,13 @@ int main(int argc, char *argv[]) {
   char old_string[512];
 
   strcpy(old_string, "\0");
-  for (int i = 0; i < argc; i++) {
-    printf("%d. Argument: %s\n", i+1, argv[i]);
+  for (int i = 1; i < argc; i++) {
+    printf("%d. Argument: '%s'\n", i, argv[i]);
     strcat(old_string, argv[i]);
   }
 
   trim(old_string, new_string);
-  printf("%s\n", new_string);
+  printf("All arguments together and trimmed: '%s'\n", new_string);
 
   return 0;
 }
